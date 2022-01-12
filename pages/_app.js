fd0bc4b03dfb7f1a57a3969f157e96201e5a7d33
Quang Truong Phan <pqtruong123@gmail.com>
2022-01-12 20:12:30 +0700

    change source css

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,27 @@
+import '../public/css/Aboutus.css';
 import '../public/css/App.css';
-import '../public/css/Header.css';
-import '../public/css/giohang.css';
-import '../public/css/Pagination.css'
-import '../public/css/ThongKe.css';
-import '../public/css/TrangChu.css';
-import '../public/css/cssDialogAlert/dialogAlert.css'
-import '../public/css/cssThongKe/tK.css'
-import '../public/css/dathangnhanh.css'
-import '../public/css/tieptucdathang.css'
-import '../public/css/themsp.css'
-import '../public/css/donhangoff.css'
+import '../public/css/Baiviet.css';
+import '../public/css/banhangcungthuocsi.css';
+import '../public/css/chitietCB.css';
+import '../public/css/combouudai.css';
+import '../public/css/dathangnhanh.css';
+import '../public/css/dialogAlert.css';
+import '../public/css/donhangoff.css';
 import '../public/css/footer.css';
-import '../public/css/giohang.css'
+import '../public/css/giohang.css';
+import '../public/css/Header.css';
 import '../public/css/index.css';
+import '../public/css/Introduce.css';
+import '../public/css/Pagination.css';
+import '../public/css/phantrang.css';
+import '../public/css/qq.css';
 import '../public/css/sanpham.css';
-import '../public/css/tktk.css'
+import '../public/css/themsp.css';
+import '../public/css/ThongKe.css';
+import '../public/css/tieptucdathang.css';
+import '../public/css/tK.css';
+import '../public/css/tktk.css';
+import '../public/css/TrangChu.css';
 
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
